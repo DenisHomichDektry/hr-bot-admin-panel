95c2ce34088d05320bfb02a3d5202acb999fb778
Denis <dzianis.khomich@dektry.com>
2023-11-21 11:11:20 +0100

    clean up

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import App from "./App.tsx";
+import "./index.css";
+import theme from "./theme.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-)
+);
---
+++ src/theme.tsx
@@ -0,0 +1,20 @@
+import { createTheme } from "@mui/material/styles";
+
+// A custom theme for this app
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#1976d2",
+    },
+    secondary: {
+      main: "#9c27b0",
+    },
+    background: {
+      default: "#fff",
+      paper: "#fff",
+    },
+  },
+});
+
+export default theme;
